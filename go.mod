48342cbf681425862d9bdf513a98e7460f801c95
long <a1234_1123@qq.com>
2026-07-29 17:11:19 +0800

    Init Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/23jdd/mrpc
+
+go 1.25.1
---
+++ protocol.go
@@ -0,0 +1,12 @@
+package mrpc
+type Request struct {
+    ServiceMethod string // "Calculator.Add"
+    Seq           uint64 // 请求 ID，用于异步匹配
+    Argv          []byte // 序列化后的参数
+}
+
+type Response struct {
+    Seq   uint64 // 对应请求 ID
+    Reply []byte // 序列化后的返回值
+    Error string // 错误信息
+}
