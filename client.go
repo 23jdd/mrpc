8d9627251eb0bfb9738a0a4874d35bea2a165f39
long <a1234_1123@qq.com>
2026-07-29 21:40:49 +0800

    feat: Add Client Module

--- client.go
+++ client.go
@@ -5,20 +5,23 @@ import (
 	"fmt"
 	"net"
 )
-var(
-	   CloseError=errors.New("Connect has been closed")  
+
+var (
+	CloseError = errors.New("Connect has been closed")
 )
+
 type Client struct {
 	address string
 	port    int
 	con     net.Conn
+	codec   Codec
 }
 
 func NewClinet(address string, port int) *Client {
-	return &Client{address: address, port: port}
+	return &Client{address: address, port: port, codec: NewMsgCodec()}
 }
 
-func (c *Client) Conn() error {
+func (c *Client) conn() error {
 	con, err := net.Dial("tcp", fmt.Sprintf("%s:%d", c.address, c.port))
 	if err != nil {
 		return err
@@ -26,12 +29,24 @@ func (c *Client) Conn() error {
 	c.con = con
 	return nil
 }
-func (c *Client) Close()error{
+func (c *Client) Close() error {
 	if c.con != nil {
-           return c.con.Close()
+		return c.con.Close()
 	}
 	return CloseError
 }
-func (c*Client)Call(name string){
-	 
+func (c *Client) Call(method string, argc any) (*Response, error) {
+	err := c.conn()
+	if err != nil {
+		return nil, err
+	}
+	buff, err := c.codec.Encode(argc)
+	if err != nil {
+		return nil, err
+	}
+	err = SendRequest(c.con, NewRequest(method, 0, buff))
+	if err != nil {
+		return nil, err
+	}
+	return ReceiveResponse(c.con)
 }
